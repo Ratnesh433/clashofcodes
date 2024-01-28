4980e511d4abe4abac1f960ed6f6668c96a6b2df
thisisnihal <mailfornihalkr@gmail.com>
2024-01-28 16:13:57 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+let menu_visibility = false;
+
+window.onscroll = () => {
+    if (screen.width <= 700) {
+        const menu_check_box = document.getElementById("checkbox-menu");
+        menu_check_box.checked = false;
+    }
+}
+
+
+window.onclick = (event) => {
+    const navlinks = document.querySelector(".navlinks");
+        
+}
+
+
